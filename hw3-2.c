63ce093ecc26c05239bb76dc73ccaa8d666d1530
azail8391 <146065149+azail8391@users.noreply.github.com>
2023-10-20 20:10:23 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ main() {
+ int s1,d1,s2,d2,s3,d3;
+    scanf("%d%d%d%d%d%d", &s1,&d1,&s2,&d2,&s3,&d3);
+    int car=1;
+    if (d1>s2||d1>s3){
+            car+=1;
+
+        if (d1>s3){
+
+            car+=1;
+            printf("%d",car);
+        }
+        else {
+
+            printf("%d",car);
+
+        }
+
+
+
+    }
+    else if (d1>s2&&d1>s3) {
+         car+=2;
+
+
+
+
+            printf("%d",car);
+
+
+
+
+    }
+
+
+
+    else {
+
+         if (d2>s3){
+
+            car+=1;
+            printf("%d",car);
+        }
+        else {
+
+            printf("%d",car);
+
+        }
+
+
+
+    }
+
+
+}
 
