851fc198decbb74044c3489b29793bcee0555ab3
azail8391 <146065149+azail8391@users.noreply.github.com>
2023-10-20 18:55:35 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+ main() {
+bool kd(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 == 0 || n % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+
+    int num;
+    scanf("%d", &num);
+    if (kd(num)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
+    }
+    return 0;
+}
