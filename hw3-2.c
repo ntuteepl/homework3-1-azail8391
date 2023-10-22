00a2251cbf822bbb01db02da9b95ccfdf2bd0659
azail8391 <146065149+azail8391@users.noreply.github.com>
2023-10-22 10:50:25 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -3,42 +3,53 @@
 #include <stdbool.h>
  main()    {
     int s1, d1, s2, d2, s3, d3;
-    int count, n_use = 0;
+    int c, n = 0;
     scanf("%d%d%d%d%d%d", &s1, &d1,&s2, &d2, &s3, &d3);
 
-    for(int i = 1; i < 25 ; i++){
-        if(i == d1){
-            n_use += 1;
-        }
+   for(int i = 1; i < 25 ; i++){
+         if(i == d1){
+            n += 1;
+
+         
+            }
         if(i == d2){
-            n_use += 1;
-        }
+            n += 1;
+           }
         if(i == d3){
-            n_use += 1;
+         
+         n += 1;
         }
 
         if(i == s1){
             if(n_use == 0){
-                count += 1;
-            }else{
-                n_use -= 1;
+                c += 1;
+            }
+            else{
+              n -= 1;
             }
         }
         if(i == s2){
-            if(n_use == 0){
-                count += 1;
-            }else{
-                n_use -= 1;
+            if(n == 0){
+               c += 1;
+            }
+            else{
+               n -= 1;
             }
         }
         if(i == s3){
-            if(n_use == 0){
-                count += 1;
-            }else{
-                n_use -= 1;
-            }
-        }
-    }
+            
+         
+            if(n == 0){
+                c += 1;
+              }
+              else{
+               
+             n -= 1;
+             }
+         }
+      }
 
-    printf("%d\n", count);
-}
+    printf("%d\n", c);
+
+ 
+ }
